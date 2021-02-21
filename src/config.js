1950164cad6a1626958c4e3935d1a08ca86c4cb3
Wayne <Waynebeckom@gmail.com>
2021-02-21 07:50:00 -0500

    My links
    Swapped out links for site.

--- src/config.js
+++ src/config.js
@@ -1,22 +1,14 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'Waynebeckom@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/beckomw',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/beckomw/',
     },
     {
       name: 'Codepen',
